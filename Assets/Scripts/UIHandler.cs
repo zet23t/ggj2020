19aaea7846553e09cd872db2420b80861c111968
Torben Binder <t.binder@steinberg.de>
2020-01-31 23:22:17 +0100

    Re-Add missing UIHandler

---
+++ Assets/Scripts/UIHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIHandler : MonoBehaviour
+{
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    private void LoadLevel(int levelIndex)
+    {
+        SceneManager.LoadScene("Scenes/Levels/Level" + levelIndex, LoadSceneMode.Single);
+    }
+
+    public void LoadFirstLevel()
+    {
+        LoadLevel(1);
+    }
+    
+    public void LoadStartingScreen()
+    {
+        SceneManager.LoadScene("Scenes/StartingScene", LoadSceneMode.Single);
+    }
+    
+    public void LoadCreditsScreen()
+    {
+        SceneManager.LoadScene("Scenes/Credits", LoadSceneMode.Single);
+    }
+    
+}
