bcabd48dd72afbd37dbc653b96e19e2d2fb9bc5e
Torben Binder <t.binder@steinberg.de>
2020-02-01 00:26:04 +0100

    Add missing Releasio function

---
+++ Assets/Scripts/Releasio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Releasio : MonoBehaviour
+{
+    public Material ReleasioMaterial;
+    private bool materialsExchanged = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && !materialsExchanged)
+        {
+            Renderer[] allRenderers = (Renderer[]) FindObjectsOfType(typeof(Renderer));
+
+            foreach (var renderer in allRenderers)
+            {
+                renderer.material = ReleasioMaterial;
+            }
+
+            materialsExchanged = true;
+        }
+    }
+}
