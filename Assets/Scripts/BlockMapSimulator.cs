e0ab30e20f5c22caa851310821b06bd4016d01c6
Eike <zet23t@googlemail.com>
2020-01-31 21:05:57 +0100

    more things

---
+++ Assets/Scripts/BlockMapSimulator.cs
@@ -0,0 +1,5 @@
+// simulates a game
+public class BlockMapSimulator {
+    public int width, height;
+    public BlockRegistry Block;
+}
--- Assets/Scripts/IBlockMap.cs
+++ Assets/Scripts/IBlockMap.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum BlockOrientation{
-
+    O0, O90, O180, O270,
+    M0, M90, M180, M270
 }
 
 public class BlockPlacement
 {
+    public int BlockId;
     public Block Block;
     BlockOrientation Orientation;
 }
