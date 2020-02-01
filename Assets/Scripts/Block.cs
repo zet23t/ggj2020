16ba40da135c871f9fa77ef59ba4269f536e1fba
Markellus <gmx@marcel-bulla.de>
2020-02-01 12:29:57 +0100

    Fixes

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -35,14 +35,14 @@ public class Block : ScriptableObject {
     public Block GetRotatedBlock(BlockOrientation orientation)
     {
         Block blockR = CreateInstance<Block>();
-        blockR.fields = fields;
+        blockR.fields = new bool[width * height];
         blockR.width = width;
         blockR.height = height;
         blockR.Prefab = Prefab;
 
-        int iRotate = 0;
+        int iRotate = (int)orientation;
         
-        bool[] fieldsThis = fields;
+        bool[] fieldsMirrored = new bool[width * height];
 
         //Mirror Block
         if ((int) orientation >= 3)
@@ -51,49 +51,73 @@ public class Block : ScriptableObject {
             {
                 for (int y = 0; y < height; y++)
                 {
-                    fieldsThis[y * height + x] = fields[y * height + (width - x)];
+                    //Debug.Log($"Mirror: {width}:{height} - {y * width + x} -> {y * width + (width - x - 1)}");
+                    fieldsMirrored[y * width + x] = fields[y * width + (width - x - 1)];
                 }
             }
+
+            iRotate -= 4;
+        }
+        else
+        {
+            fieldsMirrored = fields;
         }
 
-        //Rotation works in 90°-Steps, determine how often we have to do this
-        switch (orientation)
-        {
-            case BlockOrientation.O0:
-            case BlockOrientation.M0:
-                iRotate = 0;
-                break;
-            case BlockOrientation.O90:
-            case BlockOrientation.M90:
-                iRotate = 1;
-                break;
-            case BlockOrientation.O180:
-            case BlockOrientation.M180:
-                iRotate = 2;
-                break;
-            case BlockOrientation.O270:
-            case BlockOrientation.M270:
-                iRotate = 3;
-                break;
-        }
+        // //Rotation works in 90°-Steps, determine how often we have to do this
+        //
+        // if((int)orientation > )
+        // switch (orientation)
+        // {
+        //     case BlockOrientation.O0:
+        //     case BlockOrientation.M0:
+        //         iRotate = 0;
+        //         break;
+        //     case BlockOrientation.O90:
+        //     case BlockOrientation.M90:
+        //         iRotate = 1;
+        //         break;
+        //     case BlockOrientation.O180:
+        //     case BlockOrientation.M180:
+        //         iRotate = 2;
+        //         break;
+        //     case BlockOrientation.O270:
+        //     case BlockOrientation.M270:
+        //         iRotate = 3;
+        //         break;
+        // }
         
         //Rotate block
-        for (int i = 0; i < iRotate; i++)
+        for (int i = 0; i < fields.Length; i++)
         {
-            blockR.width = height;
-            blockR.height = width;
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    blockR.fields[y * height + x] = fieldsThis[(width - y - 1) * height + x];
-                }
-            }
-
-            fieldsThis = blockR.fields;
+            Debug.Log($"Rotate Index: {i},{iRotate}->{blockR.RotateIndex(i, iRotate)}");
+            blockR.fields[blockR.RotateIndex(i, iRotate)] = fieldsMirrored[i];
         }
 
         return blockR;
+    }
+
+    public int RotateIndex(int index, int rotation90)
+    {
+        if (rotation90 == 0)
+        {
+            return index;
+        }
+        
+        int x = 0;
+        int y = 0;
+
+        while (index > width)
+        {
+            y++;
+            index -= width;
+        }
+        y = index;
+
+        int newX = height - y - 1;
+        int newY = x;
+        int newIndex = width * newY + newX;
+        
+        return RotateIndex(newIndex, --rotation90);
     }
 
     public override string ToString()
