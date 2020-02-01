59efebb24b6e28ae9621337c657d1d6952cf9499
Eike <zet23t@googlemail.com>
2020-02-01 16:29:42 +0100

    Level pattern - not sure if useful

---
+++ Assets/Scripts/LevelPattern.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelPattern", menuName = "GJ2020/LevelPattern", order = 0)]
+public class LevelPattern : ScriptableObject {
+    public BlockRegistry BlockRegistry;
+    public BlockMaterialRegistry BlockMaterialRegistry;
+
+    
+}
