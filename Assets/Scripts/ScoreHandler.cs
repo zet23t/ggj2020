9280ea1fa60b7296dbe8e8b8a2ae66b06e341db3
Max Bebök <beboek.max@gmail.com>
2020-02-01 20:57:55 +0100

    added scrore game over display

--- Assets/Scripts/ScoreHandler.cs
+++ Assets/Scripts/ScoreHandler.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class ScoreHandler : MonoBehaviour
 {
     private AudioSource sfxBleep;
+    private bool textShowsGameOver = false;
+
     public TextMeshPro scoreText;
 
     public bool isFrozen = false;
@@ -25,12 +27,17 @@ public class ScoreHandler : MonoBehaviour
         if(!isFrozen) {
             currentScore = (long)(elapsedTime * 100);
             scoreText.text = currentScore.ToString();
-        } else if(elapsedTime > 0.5f) {
+        } else if(elapsedTime > 0.65f) {
             elapsedTime = 0.0f;
-            scoreText.text = scoreText.text == "" ? currentScore.ToString() : "";
-            if(scoreText.text != "") {
+
+            if(scoreText.text == "") {
+
+                scoreText.text = textShowsGameOver ? "SCORE" : currentScore.ToString();
+                textShowsGameOver = !textShowsGameOver;
                 sfxBleep.Play();
                 sfxBleep.volume = sfxBleep.volume * 0.9f;
+            } else {
+                scoreText.text = "";
             }
         }
     }
