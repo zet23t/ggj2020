8f14229fa4167428340648ed85c26938ae29ce73
Max Bebök <beboek.max@gmail.com>
2020-02-02 09:40:18 +0100

    fixed camera in title screen

---
+++ Assets/Scripts/RandomShake.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomShake : MonoBehaviour
+{
+    Vector3 orgPosition;
+    public float power = 0.001f;
+    float t = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orgPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        transform.position = new Vector3(
+            transform.position.x,
+            transform.position.y + (Mathf.Sin(t * 0.5f) * power),
+            transform.position.z
+        );
+
+        if((UnityEngine.Random.Range(0, 10) == 0)) {
+            t += 0.1f;
+        }
+    }
+}
